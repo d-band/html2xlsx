832198828fa607e8bcd648b00c425edfbc9a540f
振震 <865702942@qq.com>
2016-12-01 00:56:43 +0800

    add style

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import xlsx from 'better-xlsx';
 import juice from 'juice';
 import cheerio from 'cheerio';
+import { color2argb, size2pt, css2style, getBorder } from './lib';
 
 module.exports = (html, callback, options = {}) => {
   juice.juiceResources(html, options.juice || {}, (err, text) => {
@@ -15,8 +16,65 @@ module.exports = (html, callback, options = {}) => {
       $('tr', table).each((hi, th) => {
         const row = sheet.addRow();
         $('th, td', th).each((di, td) => {
+          const $td = $(td);
           const cell = row.addCell();
-          cell.value = $(td).text();
+          const css = css2style($td.css());
+
+          cell.value = $td.text();
+          const style = new xlsx.Style();
+          // Font
+          style.font.color = color2argb(css.color || '#000');
+          style.font.size = size2pt(css.fontSize);
+          style.font.name = css.fontFamily || 'Verdana';
+          style.font.bold = css.fontWeight === 'bold';
+          style.font.italic = css.fontStyle === 'italic';
+          style.font.underline = css.textDecoration === 'underline';
+          // Fill
+          const bgColor = css.backgroundColor;
+          if (bgColor) {
+            style.fill.patternType = 'solid';
+            style.fill.fgColor = color2argb(bgColor);
+          }
+          // Border
+          const left = getBorder(css, 'left');
+          if (left) {
+            style.border.left = left.style;
+            style.border.leftColor = left.color;
+          }
+          const right = getBorder(css, 'right');
+          if (right) {
+            style.border.right = right.style;
+            style.border.rightColor = right.color;
+          }
+          const top = getBorder(css, 'top');
+          if (top) {
+            style.border.top = top.style;
+            style.border.topColor = top.color;
+          }
+          const bottom = getBorder(css, 'bottom');
+          if (bottom) {
+            style.border.bottom = bottom.style;
+            style.border.bottomColor = bottom.color;
+          }
+          // Align
+          const hMap = {
+            left: 'left',
+            right: 'right',
+            center: 'center',
+            justify: 'justify'
+          };
+          if (css.textAlign && hMap[css.textAlign]) {
+            style.align.h = hMap[css.textAlign];
+          }
+          const vMap = {
+            top: 'top',
+            bottom: 'bottom',
+            middle: 'center'
+          };
+          if (css.verticalAlign && vMap[css.verticalAlign]) {
+            style.align.v = vMap[css.verticalAlign];
+          }
+          cell.style = style;
         });
       });
       index++;
---
+++ src/lib.js
@@ -0,0 +1,70 @@
+import color from 'tinycolor2';
+import { CSSStyleDeclaration } from 'cssstyle';
+
+export function color2argb (c) {
+  const rgba = color(c).toHex8();
+  return rgba.substr(6) + rgba.substr(0, 6);
+}
+
+export function size2pt (s) {
+  const num = size2px(s);
+  if (num > 0) {
+    return num * 72 / 96;
+  }
+  return 12;
+}
+
+export function size2px (s) {
+  if (!s) return 0;
+
+  const pt = s.match(/(\d+)pt/i);
+  if (pt && pt.length === 2) {
+    return parseFloat(pt[1]) * 96 / 72;
+  }
+  const em = s.match(/(\d+)em/i);
+  if (em && em.length === 2) {
+    return parseFloat(em[1]) * 16;
+  }
+  const px = s.match(/(\d+)px/i);
+  if (px && px.length === 2) {
+    return parseFloat(px[1]);
+  }
+  const pe = s.match(/(\d+)%/i);
+  if (pe && pe.length === 2) {
+    return (parseFloat(pe[1]) / 100) * 16;
+  }
+  return 0;
+}
+
+export function css2style (css = {}) {
+  const style = new CSSStyleDeclaration();
+  for (const k of Object.keys(css)) {
+    style[k] = css[k];
+  }
+  return style;
+}
+
+export function getBorder (css, type) {
+  let color = css[`border-${type}-color`];
+  let style = css[`border-${type}-style`];
+  let width = css[`border-${type}-width`];
+
+  if (!color) return null;
+
+  width = size2px(width);
+  if (width <= 0) return null;
+
+  color = color2argb(color);
+
+  if (style === 'dashed' || style === 'dotted' || style === 'double') {
+    return { style, color };
+  }
+  style = 'thin';
+  if (width >= 3 && width < 5) {
+    style = 'medium';
+  }
+  if (width >= 5) {
+    style = 'thick';
+  }
+  return { style, color };
+}
